e6a9793fc50b53747c417d9dea369103b7afe156
babim <captainprice1356@gmail.com>
2023-07-19 15:04:44 +0300

    в main py  реализована валидация + енам
    нужно дописать ограничения условно имя не боле 20 символов и т.ж
    
    app половинка загрушки для алены
    
    с main py на app приходит json
    
    нужно понять как его починить, но пока все работает

--- app/backend/main.py
+++ app/backend/main.py
@@ -4,6 +4,9 @@ import uvicorn
 import asyncio
 import datetime
 import os
+from pydantic import BaseModel, Field
+from enum import Enum, IntEnum
+import aiohttp
 
 DEFAULT_FRONTEND_HOST = '127.0.0.1'
 DEFAULT_FRONTEND_PORT = '50422'
@@ -35,8 +38,8 @@ async def startup_event():
 async def shutdown_event():
     print('Server shutdown :', datetime.datetime.now())
 
-
-@app.post("/")
+## настин запрос
+@app.post("/123")
 async def get_form_data(info: Request):
     req_info = await info.json()
     print(req_info)
@@ -44,7 +47,138 @@ async def get_form_data(info: Request):
         "status": "SUCCESS",
         "data": req_info
     }
+class FamilyStatus(str, Enum):
+    Single = 'Single / not married'
+    Married = 'Married'
+    Civil_marriage = 'Civil marriage'
+    Separated = 'Separated'
+    Widow = 'Widow'
 
+class HousingType(str, Enum):
+    House_apartment = 'House / apartment'
+    With_parents = 'With parents'
+    Municipal_apartment = 'Municipal apartment'
+    Rented_apartment = 'Rented apartment'
+    Office_apartment = 'Office apartment'
+    Co_op_apartment = 'Co-op apartment'
+class EducationType(str, Enum):
+    Secondary_secondary_special = 'Secondary / secondary special'
+    Higher_education = 'Higher education'
+    Incomplete_higher = 'Incomplete higher'
+    Lower_secondary = 'Lower secondary'
+    Academic_degree = 'Academic degree'
+
+class OccupationType(str, Enum):
+    Laborers = 'Laborers'
+    Core_staff = 'Core staff'
+    Accountants = 'Accountants'
+    Managers = 'Managers'
+    Drivers = 'Drivers'
+    Sales_staff = 'Sales staff'
+    Cleaning_staff = 'Cleaning staff'
+    Cooking_staff = 'Cooking staff'
+    Private_service_staff = 'Private service staff'
+    Medicine_staff = 'Medicine staff'
+    Security_staff = 'Security staff'
+    High_skill_tech_staff = 'High skill tech staff'
+    Waiters_barmen_staff = 'Waiters/barmen staff'
+    Low_skill_Laborers = 'Low-skill Laborers'
+    Realty_agents = 'Realty agents'
+    Secretaries = 'Secretaries'
+    IT_staff = 'IT staff'
+    HR_staff = 'HR staff'
+    Other_occupation = 'Other'
+
+class OrganizationType(str, Enum):
+    Business = 'Business'
+    School = 'School'
+    Government = 'Government'
+    Religion = 'Religion'
+    Other_organization = 'Other'
+    Electricity = 'Electricity'
+    Medicine = 'Medicine'
+    Self_employed = 'Self-employed'
+    Transport = 'Transport'
+    Construction = 'Construction'
+    Housing = 'Housing'
+    Kindergarten = 'Kindergarten'
+    Trade = 'Trade'
+    Industry = 'Industry'
+    Military = 'Military'
+    Services = 'Services'
+    Security_Ministries = 'Security Ministries'
+    Emergency = 'Emergency'
+    Security = 'Security'
+    University = 'University'
+    Police = 'Police'
+    Postal = 'Postal'
+    Agriculture = 'Agriculture'
+    Restaurant = 'Restaurant'
+    Culture = 'Culture'
+    Hotel = 'Hotel'
+    Bank = 'Bank'
+    Insurance = 'Insurance'
+    Mobile = 'Mobile'
+    Legal_Services = 'Legal Services'
+    Advertising = 'Advertising'
+    Cleaning = 'Cleaning'
+    Telecom = 'Telecom'
+    Realtor = 'Realtor'
+
+class IncomeType(str, Enum):
+    Student = 'Student'
+    Working = 'Working'
+    Commercial_associate = 'Commercial associate'
+    State_servant = 'State servant'
+    Pensioner = 'Pensioner'
+    Unemployed = 'Unemployed'
+class Validation(BaseModel):
+
+    SURNAME: str = Field(default="Undefined", min_length=3, max_length=20)
+    NAME: str = Field(default="Undefined", min_length=3, max_length=20)
+    PATRONYMIC: str = Field(default="Undefined", min_length=3, max_length=20)
+    # TODO:
+    CODE_GENDER: str
+    # TODO: перенести вычисления на бэк, чтобы они происходили после валидации
+    DAYS_BIRTH: int
+    DAYS_BIRTH: str
+    PASSPORT: str
+
+    NAME_FAMILY_STATUS: FamilyStatus
+    CNT_CHILDREN: int
+    NAME_HOUSING_TYPE: HousingType
+    FLAG_OWN_CAR: str
+    NAME_EDUCATION_TYPE: EducationType
+    #
+    OCCUPATION_TYPE: OccupationType
+    ORGANIZATION_TYPE: OrganizationType
+    # # TODO: перенести вычисления на бэк, чтобы они происходили после валидации
+    DAYS_EMPLOYED: int
+    # DAYS_EMPLOYED: str
+    NAME_INCOME_TYPE: IncomeType
+    # # TODO: перенести вычисления на бэк, чтобы они происходили после валидации
+    AMT_INCOME_TOTAL: float
+    # 'AMT_INCOME_TOTAL': str(float(self.income_total.input.value) / 2),
+    #
+    AMT_CREDIT: int
+    # # TODO: перенести вычисления на бэк, чтобы они происходили после валидации
+    AMT_ANNUITY: float
+    # 'AMT_ANNUITY': str(float(self.credit.input.value) / int(self.months.input.value))
+@app.post('/')
+async def post_on_ml(info: Validation):
+    # Шаг 1: Прием валидированных данных info: Validation
+    print(info)
+
+    # Шаг 2: Асинхронная отправка POST-запроса к другому приложению ML
+    async with aiohttp.ClientSession() as session:
+        async with session.post('http://127.0.0.1:8888/') as response:
+            result = await response.json()
+
+
+async def send_to_ml(info: Validation):
+    # тут мы принимаем инфо и делаем !!! асинхронно !!! реквест на пост на ML, далее обрабатываем скоринг балл
+    # и возвращаем на фронт
+    pass
 
 if __name__ == '__main__':
     uvicorn.run("main:app", reload=True)
