84140b958b91e7c735e13867a99a82725a291eb8
LogExE <awesome149712@gmail.com>
2023-07-20 20:46:50 +0400

    tf

---
+++ terraform/instance.tf
@@ -0,0 +1,49 @@
+
+resource "yandex_compute_image" "debian11" {
+  source_family = "debian-11"
+}
+
+resource "yandex_compute_instance" "neopr" {
+  name = var.instance_name
+
+  platform_id = "standard-v3"
+  resources {
+    core_fraction = 20
+    cores         = 2
+    memory        = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = yandex_compute_image.debian11.id
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+
+  scheduling_policy {
+    preemptible = true
+  }
+
+  metadata = {
+    user-data = "${file("meta.txt")}"
+  }
+}
+
+resource "yandex_vpc_network" "network-1" {
+  name = var.network_name
+}
+
+resource "yandex_vpc_subnet" "subnet-1" {
+  name           = var.subnet_name
+  zone           = var.yandex_zone
+  network_id     = yandex_vpc_network.network-1.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
+}
+
+output "external_ip_address_neopr" {
+  value = yandex_compute_instance.neopr.network_interface.0.nat_ip_address
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,44 @@
+variable "cloud_id" {
+  type    = string
+  default = "b1gnfug9omgb3nduktd2"
+}
+
+variable "folder_id" {
+  type    = string
+  default = "b1gsdge91vukh1d8u77r"
+}
+
+variable "service_key_path" {
+  type    = string
+  default = "C:/Users/Vladimir/authorized_key.json"
+}
+
+variable "instance_name" {
+  type    = string
+  default = "neopr"
+}
+
+variable "yandex_zone" {
+  type    = string
+  default = "ru-central1-b"
+}
+
+variable "network_name" {
+  type    = string
+  default = "network1"
+}
+
+variable "subnet_name" {
+  type    = string
+  default = "subnet1"
+}
+
+variable "dns_name" {
+  type    = string
+  default = "dns1"
+}
+
+variable "dns_zone" {
+  type = string
+  default = "ourscoringsys.ru."
+}
