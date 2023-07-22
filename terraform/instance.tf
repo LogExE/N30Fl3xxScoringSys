c7373cdece72474036ccdf20cc67bc0c6b5af155
LogExE <awesome149712@gmail.com>
2023-07-22 21:09:29 +0400

    fix ip var name

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -45,6 +45,6 @@ resource "yandex_vpc_subnet" "subnet-1" {
   v4_cidr_blocks = ["192.168.10.0/24"]
 }
 
-output "external_ip_address_neopr" {
+output "external_ip" {
   value = yandex_compute_instance.neopr.network_interface.0.nat_ip_address
 }
